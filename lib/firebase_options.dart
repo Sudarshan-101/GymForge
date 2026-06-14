56e9fbd7fedfa69f34a1929c8bfd51061fbcd0e3
Sudarshan-101 <sudarshangaikhe101@gmail.com>
2026-06-14 13:57:22 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -18,33 +18,7 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCWzMixBod41zJozuGHQieYr9VDJeQtwjk',
-    appId: '1:659942518247:android:d29e87f79f3a1a4bdc6142',
-    messagingSenderId: '659942518247',
-    projectId: 'gymforge-f9fe3',
-    storageBucket: 'gymforge-f9fe3.firebasestorage.app',
-  );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCWzMixBod41zJozuGHQieYr9VDJeQtwjk',
-    appId: '1:659942518247:ios:placeholder',
-    messagingSenderId: '659942518247',
-    projectId: 'gymforge-f9fe3',
-    storageBucket: 'gymforge-f9fe3.firebasestorage.app',
-    iosClientId: '',
-    iosBundleId: 'com.gymforge.app',
-  );
-
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: "AIzaSyBjnQ9eKa5qqw9MU8EYKUBP8-7SUezQjKU",
-  authDomain: "gymforge-f9fe3.firebaseapp.com",
-  projectId: "gymforge-f9fe3",
-  storageBucket: "gymforge-f9fe3.firebasestorage.app",
-  messagingSenderId: "659942518247",
-  appId: "1:659942518247:web:ab24d33dc970411cdc6142",
-  measurementId: "G-KHB4KLPR9P"
-  );
 
 
 }
